a55c2a7370ade386b8ca369c78852c188cc18c34
John Karakasidis <john.karakasidis@outlook.com>
2023-08-07 01:31:08 +0300

    kala paei

---
+++ Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+string st = "Casting Time 1 standard action\r\n" + " Components V,S";
+
+string[] castArray = st.Split('\n');
+
+string castingTime = castArray[0].Substring(14);
+
+string components = castArray[1].Substring(11);
+
+Console.WriteLine("Casting time" + castingTime + "   "  +  "componentes" + components);
--- sele/Program.cs
+++ sele/Program.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using System.Security.Cryptography.X509Certificates;
 using System.Reflection.Metadata;
 using System.Drawing.Text;
+using OpenQA.Selenium.DevTools;
+using System.Text;
+using System.Xml.Linq;
 
 
 
@@ -20,16 +23,10 @@ SeleniumGetSpellProperties();
 Console.WriteLine("telos");
 Console.ReadLine();
 
-
-//testnig funs
-//testnig funs
-//testnig funs
-//testnig funs
-//testnig funs
-//testnig funs
-//testnig funs
 static void GetLinks(string excelFilePath)
 {
+
+
     try
     {
         using (var package = new ExcelPackage(new FileInfo(excelFilePath)))
@@ -65,10 +62,25 @@ static void SeleniumGetSpellProperties()
 {
     bool hasSpellInDesc = false;
 
+
+    string? name = "";
+    string? school = "";
+    string? level = "";
+    string? castingTime = "";
+    string? components = "";
+    string? effect = "";
+    string? range = "";
+    string? target = "";
+    string? area = "";
+    string? duration = "";
+    string? savingThrow = "";
+    string? spellResistance = "";
+    string? description = "";
+
     try
     {
         var driver = new ChromeDriver();
-        driver.Navigate().GoToUrl("https://www.d20pfsrd.com/magic/all-spells/e/envious-urge/");
+        driver.Navigate().GoToUrl("https://www.d20pfsrd.com/magic/all-spells/a/acid-pit/");
 
 
         IWebElement descriptionElement = driver.FindElement(By.ClassName("article-text"));
@@ -80,101 +92,92 @@ static void SeleniumGetSpellProperties()
         spellDescriptionArray.Add(namesh1[0].Text);
 
         //if it finds an h4 element inside the 
-        if (namesh4.Count > 0)
-        {
-            hasSpellInDesc = true;
+        //if (namesh4.Count > 0)
+        //{
+        //    hasSpellInDesc = true;
 
-            string name4 = namesh4[0].Text;
-            Console.WriteLine("There is a spell in this spell named: " + name4);
-        }
+        //    string name4 = namesh4[0].Text;
+        //    Console.WriteLine("There is a spell in this spell named: " + name4);
+        //}
 
-        int counter = 0;
-
-        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        
         foreach (var element in elements)
         {
             spellDescriptionArray.Add(element.Text);
-            Console.WriteLine( ++counter + element.Text );
         }
-        Console.WriteLine(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-
-        string? name = spellDescriptionArray[0];
-        string? school = "";
-        string? level = "";
-        string? castingTime = "";
-        string? components = "";
-        string? effect = "";
-        string? range = "";
-        string? target = "";
-        string? area = "";
-        string? duration = "";
-        string? savingThrow = "";
-        string? description = "";
+        name = spellDescriptionArray[0];
 
         int semicolonIndex = spellDescriptionArray[1].IndexOf(';');
         int castIndex = spellDescriptionArray[3].IndexOf("Casting Time ");
-
         school = spellDescriptionArray[1].Substring(0, semicolonIndex).Trim();
-
         level = spellDescriptionArray[1].Substring(semicolonIndex + 7).Trim();
+        int propLen = spellDescriptionArray.Count();
 
-
-
-
-
-
-        if (spellDescriptionArray[2].ToLower() == "casting")
+        for(int i=0; i<propLen; i++)
         {
-            string[] castArray = spellDescriptionArray[3].Split('\n');
-
-            castingTime = castArray[0].Substring("Casting Time ".Length).Trim();
-
-            components = castArray[1].Substring("Components ".Length).Trim();
-
-            //why though
-            if (spellDescriptionArray[4].ToLower() == "effect")
+            if (spellDescriptionArray[i].ToLower() == "casting")
+            {
+                string[] castArray = spellDescriptionArray[i+1].Split('\n');
+                castingTime = castArray[0].Substring(14).Trim();
+                components = castArray[1].Substring(11).Trim();
+            }
+            else if (spellDescriptionArray[i].ToLower() == "effect")
             {
                 Console.WriteLine("mpenei sto effect   ");
-                string[] effectArray = spellDescriptionArray[5].Split('\n');
+                string[] effectArray = spellDescriptionArray[i+1].Split('\n');
                 int len = effectArray.Length;
 
-                for (int i = 0; i < len; i++)
+                for (int j = 0; j < len; j++)
                 {
-                    if (effectArray[i].ToLower().Contains("range"))
+                    if (effectArray[j].ToLower().Contains("range"))
                     {
-                        range = effectArray[i].Substring(5);
+                        range = effectArray[j].Substring(5).Trim();
                     }
-                    else if (effectArray[i].ToLower().Contains("area"))
+                    else if (effectArray[j].ToLower().Contains("area"))
                     {
-                        area = effectArray[i].Substring(4);
+                        area = effectArray[j].Substring(4).Trim();
                     }
-                    else if (effectArray[i].ToLower().Contains("effect"))
+                    else if (effectArray[j].ToLower().Contains("effect"))
                     {
-                        effect = effectArray[i].Substring(6);
+                        effect = effectArray[j].Substring(6).Trim();
                     }
-                    else if (effectArray[i].ToLower().Contains("targets"))
+                    else if (effectArray[j].ToLower().Contains("targets"))
                     {
-                        target = effectArray[i].Substring(7);
+                        target = effectArray[j].Substring(7).Trim();
                     }
-                    else if (effectArray[i].ToLower().Contains("target"))
+                    else if (effectArray[j].ToLower().Contains("target"))
                     {
-                        target = effectArray[i].Substring(6);
+                        target = effectArray[j].Substring(6).Trim();
                     }
-                    else if (effectArray[i].ToLower().Contains("duration"))
+                    else if (effectArray[j].ToLower().Contains("duration"))
                     {
-                        duration = effectArray[i].Substring(8);
+                        duration = effectArray[j].Substring(8).Trim();
                     }
-                    else if (effectArray[i].ToLower().Contains("saving throw"))
+                    else if (effectArray[j].ToLower().Contains("saving throw"))
                     {
+                        int index = effectArray[j].IndexOf("Spell Resistance");
+                        if (index != -1)
+                        {
+                            string saveString = effectArray[j].Substring(0, index).Trim();
+                            string SRString = effectArray[j].Substring(index).Trim();
 
+                            savingThrow = saveString;
+                            spellResistance = SRString;
+                        }
                     }
                 }
             }
+            else if (spellDescriptionArray[i].ToLower() == "description")
+                {
+                    description = spellDescriptionArray[i+1];
+                }
         }
 
 
 
+
+        Console.WriteLine(" KSEKINIMA ");
         Console.WriteLine("Name: " + name);
         Console.WriteLine("School: " + school);
         Console.WriteLine("Level: " + level);
@@ -184,13 +187,9 @@ static void SeleniumGetSpellProperties()
         Console.WriteLine("Target: " + target);
         Console.WriteLine("Duration: " + duration);
         Console.WriteLine("Saving Throw: " + savingThrow);
+        Console.WriteLine("Spell Resistance: " + spellResistance);
         Console.WriteLine("Description: " + description);
 
-
-        //foreach (string element in strings)
-        //{
-        //        Console.WriteLine(element);
-        //}
 
         driver.Quit();
     }
@@ -199,5 +198,7 @@ static void SeleniumGetSpellProperties()
         Console.WriteLine(ex.Message);
         throw;
     }
+
+    
 }
 
